770525378b4a16fcb3f66466a7d5b3450053cf51
Harsh <patelharsh94@gmail.com>
2019-08-31 13:00:18 -0500

    initial import

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String [] args) {
 
-        int [] randomArray = {1,2,2,2,2,3,4,7};
+        int [] randomArray = {2,2,2,2,3,3,4,4};
         NumberOfOccurances numberOfOccurances = new NumberOfOccurances();
 
         System.out.println(numberOfOccurances.getNumberOfOccurances(randomArray, 2));
@@ -26,11 +26,11 @@ public class Main {
 //        minStack.top();
 //        minStack.getMin();
 
-        int [] difficulty = {68,35,52,47,86};
-        int [] profit = {67,17,1,81,3};
-        int [] worker = {92,10,85,84,82};
-
-        MostProfitAssigningWork mostProfitAssigningWork = new MostProfitAssigningWork();
-        mostProfitAssigningWork.maxProfitAssignment(difficulty, profit, worker);
+//        int [] difficulty = {68,35,52,47,86};
+//        int [] profit = {67,17,1,81,3};
+//        int [] worker = {92,10,85,84,82};
+//
+//        MostProfitAssigningWork mostProfitAssigningWork = new MostProfitAssigningWork();
+//        mostProfitAssigningWork.maxProfitAssignment(difficulty, profit, worker);
     }
 }
--- src/NumberOfOccurances.java
+++ src/NumberOfOccurances.java
@@ -3,25 +3,38 @@ public class NumberOfOccurances {
     int getNumberOfOccurances(int [] arr, int target) {
 
         int leftIndex = getSubStringIndex(arr, 0, arr.length - 1, true, target);
-        int rightIndex = getSubStringIndex(arr, leftIndex+1, arr.length - 1, false, target);
+        int rightIndex = getSubStringIndex(arr, 0, arr.length - 1, false, target);
 
-        return (rightIndex - leftIndex) + 1;
+        if (rightIndex - leftIndex > 0)
+            return (rightIndex - leftIndex) + 1;
+        else if (arr[leftIndex] == target || arr[rightIndex] == target)
+            return 1;
+        else
+            return 0;
     }
 
     int getSubStringIndex(int [] arr, int left, int right, boolean isLeft, int target) {
 
-        while (left < right) {
+        while (left <= right) {
 
-            int mid = (right - left)/2;
+            double midDouble = Math.floor(((double) right + left) / 2);
+            int mid = (int) midDouble;
 
-            if ((isLeft && arr[mid] == target && arr[mid - 1] != target) ||
+            if ((mid == 0 || mid == arr.length - 1 ) && arr[mid] == target)
+            {
+                return mid;
+            } else if ((isLeft && arr[mid] == target && arr[mid - 1] != target) ||
                     (!isLeft && arr[mid] == target && arr[mid + 1] != target))
             {
                 return mid;
             } else if (arr[mid] < target) {
-                right = mid - 1;
-            } else {
                 left = mid + 1;
+            } else if (arr[mid] > target){
+                right = mid - 1;
+            } else if (isLeft) {
+                right = mid;
+            } else {
+                left = mid;
             }
         }
 
