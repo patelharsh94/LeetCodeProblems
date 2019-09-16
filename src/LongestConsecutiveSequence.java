a4f20a9d00f346e3d906e63d03d850e392984189
Harsh Patel <patelharsh94@gmail.com>
2019-09-16 14:17:21 -0500

    128. Longest Consecutive Sequence

---
+++ src/LongestConsecutiveSequence.java
@@ -0,0 +1,38 @@
+import java.util.HashSet;
+
+public class LongestConsecutiveSequence {
+
+    public int longestConsecutive(int[] nums) {
+
+        HashSet<Integer> numsSet = new HashSet<>();
+        int max = 0;
+        int seqSize;
+
+        for (int i = 0; i < nums.length; i++)
+            numsSet.add(nums[i]);
+
+
+        for (int i = 0; i < nums.length; i++) {
+            seqSize = 1;
+            numsSet.remove(nums[i]);
+
+            int low = nums[i];
+            int high = nums[i];
+
+            while (numsSet.contains(--low)) {
+                numsSet.remove(low);
+                seqSize++;
+            }
+
+            while (numsSet.contains(++high)) {
+                numsSet.remove(high);
+                seqSize++;
+            }
+
+            if (seqSize > max)
+                max = seqSize;
+        }
+
+        return max;
+    }
+}
