0659557ea25e93786e883543fee1d1367984dcca
Harsh Patel <patelharsh94@gmail.com>
2024-06-26 20:22:53 -0500

    leet code problems initial push

--- src/Java/Algorithms/BinarySearch.java
+++ src/Java/Algorithms/BinarySearch.java
@@ -1,27 +1,31 @@
 package Algorithms;
 
+import org.junit.Test;
+
 public class BinarySearch {
 
-    public int binarySearchIterative (int x, int [] data) {
-
-        int mid = data.length/2;
+    public int binTest(int x, int [] data) {
         int start = 0;
         int end = data.length;
+        int mid = (end+start)/2;
 
         while (start < end) {
 
             if (data[mid] == x) {
                 return mid;
-            } else if (x > data[mid]) {
+            }
+
+            if (data[mid] < x) {
                 start = mid + 1;
             } else {
                 end = mid;
             }
-            mid = (end + start) / 2;
-        }
 
-        return -1;
+            mid = (end + start)/2;
+
+        }
     }
+
 
     public int binarySearchRecursive (int x, int [] data) {
         return binarySearchRec(0, data.length, data, x);
@@ -39,5 +43,14 @@ public class BinarySearch {
             return binarySearchRec(mid+1, end, data, val);
         else
             return binarySearchRec(start, mid, data, val);
+    }
+
+    @Test
+    public void test() {
+        int [] data = new int[] {6, 7, 12, 14, 15, 23, 32, 37, 39};
+
+        int index = binarySearchIterative(12, data);
+
+        System.out.println(index);
     }
 }
